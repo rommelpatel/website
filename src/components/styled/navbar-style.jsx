d5be5eb137f233586244a8bd082c62cd328fd817
rommelpatel <rommelpa@usc.edu>
2022-11-17 19:39:21 -0800

    change color palette

--- src/components/styled/navbar-style.jsx
+++ src/components/styled/navbar-style.jsx
@@ -3,14 +3,14 @@ import styled from "styled-components";
 import { Link } from "react-router-dom";
 
 export const TopNav = styled.div`
-    background-color: #333;
+    background-color: #9c9c9c;
     overflow: hidden;
 `;
 
 export const NavButton = styled(Link)`
     float: right;
     display: block;
-    color: #f2f2f2;
+    color: #030303;
     text-align: center;
     padding: 14px 16px;
     text-decoration: none;
