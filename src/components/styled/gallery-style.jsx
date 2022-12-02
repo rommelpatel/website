985ca6cf90610f96dc87e80a212cdd1d658cd812
rommelpatel <rommelpa@usc.edu>
2022-12-01 18:02:52 -0800

    initial gallery content

--- src/components/styled/gallery-style.jsx
+++ src/components/styled/gallery-style.jsx
@@ -1,8 +1,37 @@
 import React from "react";
 import styled from "styled-components";
 
-const GalleryStyle = styled.div`
-    color: #030303;
+export const GalleryStyle = styled.div`
+    color: #ffffff;
+    padding: 10px 16px;
+    font-size: 25px;
+    font-family: "Courier New", Courier, monospace;
 `;
 
-export default GalleryStyle;
+export const Row = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    padding: 0 4px;
+`;
+
+export const Column = styled.div`
+    flex: 25%;
+    max-width: 25%;
+    padding: 0 4px;
+
+    @media screen and (max-width: 800px) {
+        flex: 50%;
+        max-width: 50%;
+    }
+
+    @media screen and (max-width: 600px) {
+        flex: 100%;
+        max-width: 100%;
+    }
+`;
+
+export const ImageStyle = styled.img`
+    margin-top: 8px;
+    vertical-align: middle;
+    width: 100%;
+`;
--- src/pages/gallery.jsx
+++ src/pages/gallery.jsx
@@ -1,8 +1,27 @@
 import React from "react";
-import GalleryStyle from "../components/styled/gallery-style";
+import {
+    GalleryStyle,
+    Row,
+    Column,
+    ImageStyle,
+} from "../components/styled/gallery-style";
+import bambi from "../../images/deer-edit-cont.jpg";
+import marina from "../../images/marina-edit-100.jpg";
 
 function Gallery() {
-    return <GalleryStyle>Gallery</GalleryStyle>;
+    return (
+        <div>
+            <GalleryStyle>Gallery</GalleryStyle>
+            <Row>
+                <Column>
+                    <ImageStyle src={bambi} />
+                </Column>
+                <Column>
+                    <ImageStyle src={marina} />
+                </Column>
+            </Row>
+        </div>
+    );
 }
 
 export default Gallery;
